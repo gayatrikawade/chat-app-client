82b92323695713e3e689da3db38433c6d1ac8209
Gayatri Kawade <gayatrikawade80@gmail.com>
2020-09-13 21:31:53 +0530

    done: message send and receive

--- src/app/chat-box/chat-box.component.ts
+++ src/app/chat-box/chat-box.component.ts
@@ -11,6 +11,7 @@ const SOCKET_ENDPOINT = 'localhost:3000';
 export class ChatBoxComponent implements OnInit {
 
   socket;
+  message: string;
   constructor() { }
 
   ngOnInit() {
@@ -19,5 +20,27 @@ export class ChatBoxComponent implements OnInit {
 
   setupScoketConnection() {
     this.socket = io(SOCKET_ENDPOINT);
+    this.socket.on('message-broadcast', (data: string) => {
+      if (data) {
+        const element = document.createElement('li');
+        element.innerHTML = data;
+        element.style.background = 'white';
+        element.style.padding = '15px 30px';
+        element.style.margin = '10px';
+        document.getElementById('message-list').appendChild(element);
+      }
+    });
+  }
+
+  sendMessage() {
+    this.socket.emit('message', this.message);
+    const element = document.createElement('li');
+    element.innerHTML = this.message;
+    element.style.background = 'white';
+    element.style.padding = '15px 30px';
+    element.style.margin = '10px';
+    element.style.textAlign = 'right';
+    document.getElementById('message-list').appendChild(element);
+    this.message = '';
   }
 }
